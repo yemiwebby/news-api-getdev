83ac4921a7328c9f9aae3b5391ae76bb72580528
yemiwebby <yemiwebby@gmail.com>
2018-12-04 16:49:11 +0100

    created the entry file and imported express and other important modules

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './src/routes/index';
+
+
+const app = express();
+
+// PORT 
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+// logger
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+// Routes
+routes(app);
+
+// Wildcard route
+app.get('/*', (req, res) => {
+    res.status(404).json({
+        message: 'Resource not found'
+    })
+})
+
+// Listen on a port
+app.listen(port);
+
+export default app;
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+
+
+
+export default (app) => {
+    app.get('/sample');
+}
